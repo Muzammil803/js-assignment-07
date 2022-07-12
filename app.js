b2da562291c3e6240d9956a133899d1bc34483b1
Muzammil803 <syedmuzammil691@gmail.com>
2022-07-12 11:45:38 +0500

    assignment complete

---
+++ app.js
@@ -0,0 +1,67 @@
+// var userinput1 = prompt("first name")
+// var userinput2 = prompt("last name")
+// var fullname = userinput1 +" "+ userinput2
+
+// document.write(fullname.length)
+
+// =============================================
+// Q 03 
+// =========================
+
+
+// var input = prompt("enter name")
+// var input2 = prompt("find a alphabet")
+// var size = input.lastIndexOf(input2)
+// document.write(size)
+
+// =============================
+// Q 05/''
+// ==========================
+// var input = prompt("enter")
+// var index =+prompt("index num")
+// var result = input[index]
+// document.write(result)
+
+// =================
+// Q 07
+// ===============
+
+// var text = "my name is muzammil. muzammil is a wb developer . muzammil is also a nice guy"
+
+// text1 =text.replace(/muzammil/g,"anwar")
+    
+// document.write(text1)
+
+
+
+// var a = 2.33
+// var b = a.toFixed(2)
+// console.log(b)
+
+// =============================
+
+// var userinput1= prompt("enter user name")
+// for (var i=0; i<userinput1.length; i++){
+
+//     if(  userinput1.slice(i,i+1) === "@" ){
+//         alert("correct")
+//     }
+//     else{
+//         alert("enter valid user name ")
+//     }
+// }
+
+
+
+// ================================
+
+
+// var password = prompt("enter password")
+
+// if (password=="A-Z" && password=="0-9" && password.length>6){
+
+
+// }
+// else{
+//     alert("enter strong password")
+// }
